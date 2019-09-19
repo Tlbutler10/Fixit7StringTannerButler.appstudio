097ce15a375366cda319efdfc25004d60e03f10b
Tanner <tannerbutler15@gmail.com>
2019-09-19 18:11:46 -0500

    Finished Question 2

--- forms/StringMethods/StringMethods.js
+++ forms/StringMethods/StringMethods.js
@@ -1,3 +1,23 @@
+//a
 let quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
+console.log(quoteString)
+//b
 let upperCaseString= quoteString.toUpperCase()
-let authorString="_Henry Ford"
+console.log(upperCaseString)
+//c
+let authorString="-Henry Ford"
+console.log(authorString)
+//d
+let completeString = quoteString.concat(authorString)
+console.log(completeString)
+//e
+let secondQuote="Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King"
+console.log(secondQuote)
+//f
+let lowerCaseString = secondQuote.toLowerCase()
+console.log(lowerCaseString)
+//g
+console.log(secondQuote.charAt(2))
+//h
+let findString = secondQuote.slice(35,-14)
+console.log(findString)
